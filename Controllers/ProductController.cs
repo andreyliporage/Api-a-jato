103b29e8e757e8d083bf4b75c042150a093afa68
andreyliporage <andreyliporage@gmail.com>
2021-03-16 19:33:30 -0300

    controller produtos working

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get([FromServices]DataContext context) {
+            var products = await context.Products
+                .Include(p => p.Category)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(products);
+        }        
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetById(int id, [FromServices]DataContext context) {
+            var product = await context.Products
+                .Include(p => p.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory(int id, [FromServices]DataContext context) {
+            var products = await context
+                .Products
+                .Include(p => p.Category)
+                .AsNoTracking()
+                .Where(p => p.Category.Id == id).ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Post([FromServices]DataContext context,
+        [FromBody]Product model) {
+            if (ModelState.IsValid) {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
